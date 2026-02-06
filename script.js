9cd8a09b54776e4d77a2f181be7978008a06b764
Koncsik Bence <koncsik.bence@BOLYAI.LOCAL>
2026-02-06 07:57:42 +0100

    commit2

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 const input1 = document.createElement('input');
 const input2 = document.createElement('input');
 const eredmenyDiv = document.createElement('div');
+
+document.body.appendChild(input1);
+document.body.appendChild(input2);
+document.body.appendChild(eredmenyDiv);
